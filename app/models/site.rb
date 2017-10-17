5aeee89ed70526aceb3d7e4d6566274fa1c71e21
Misha Slyusarev <mik3weider@gmail.com>
2017-10-17 23:39:47 +0200

    Add validations to site and tag models

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,5 +1,8 @@
 class Site < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
 
+  validates :url, presence: true
+  validates :url, http_url: true
+
   after_touch :destroy, if: -> { bookmarks.empty? }
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,8 @@
 class Tag < ApplicationRecord
   has_and_belongs_to_many :bookmarks
 
+  validates :name, presence: true
+
   before_save :normalize_name
 
   private
