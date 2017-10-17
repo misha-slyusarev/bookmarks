72fe64acbcd3dea8e4453b95042a8f0dc973a0f7
Misha Slyusarev <mik3weider@gmail.com>
2017-10-17 23:31:41 +0200

    Add tag name normalisation

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,11 @@
 class Tag < ApplicationRecord
   has_and_belongs_to_many :bookmarks
+
+  before_save :normalize_name
+
+  private
+
+    def normalize_name
+      self[:name] = name.downcase
+    end
 end
