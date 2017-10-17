6483c8986ed66068b744a461601b22694ad96d86
Misha Slyusarev <mik3weider@gmail.com>
2017-10-17 13:25:32 +0200

    Add concat_tag_list helper

--- app/helpers/bookmarks_helper.rb
+++ app/helpers/bookmarks_helper.rb
@@ -1,2 +1,10 @@
 module BookmarksHelper
+  def concat_tag_list(tag_list)
+    capture do
+      tag_list.split.each do |tag|
+        concat(content_tag(:span, tag, class: "tag label label-info"))
+        concat ' '
+      end
+    end
+  end
 end
